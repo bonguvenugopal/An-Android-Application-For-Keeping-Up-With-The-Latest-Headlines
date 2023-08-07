e4936bf8df073d9d82bbff8223cdaa5ea1dff31a
bonguvenugopal <venu80577@gmail.com>
2023-08-07 10:27:12 +0530

    This is my First Commit

---
+++ app/src/main/java/com/example/newsheadlines/ApiService.kt
@@ -0,0 +1,30 @@
+package com.example.newsheadlines
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ApiService {
+
+    //@GET("movielist.json")
+    @GET("top-headlines?country=in&apiKey=69436d6d95a241678692382582c506dc")
+    ///@GET("search?q=chatgpt")
+    suspend fun getMovies() :News
+
+    companion object {
+        var apiService: ApiService? = null
+        fun getInstance() : ApiService {
+            if (apiService == null) {
+                apiService = Retrofit.Builder()
+                    // .baseUrl("https://howtodoandroid.com/apis/")
+                    .baseUrl("https://newsapi.org/v2/")
+                    //.baseUrl("https://podcast-episodes.p.rapidapi.com/")
+
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build().create(ApiService::class.java)
+            }
+            return apiService!!
+        }
+    }
+
+}
